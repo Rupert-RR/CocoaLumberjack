207374104b3ea28dc9ab478f54144ea59f0d6dd1
Rupert <rupert@pearlshare.com>
2015-02-09 14:31:02 +0000

    Added root project

---
+++ CocoaLumberjack/CocoaLumberjack.h
@@ -0,0 +1,19 @@
+//
+//  CocoaLumberjack.h
+//  CocoaLumberjack
+//
+//  Created by Rupert on 09/02/2015.
+//  Copyright (c) 2015 CocoaLumberjack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaLumberjack.
+FOUNDATION_EXPORT double CocoaLumberjackVersionNumber;
+
+//! Project version string for CocoaLumberjack.
+FOUNDATION_EXPORT const unsigned char CocoaLumberjackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaLumberjack/PublicHeader.h>
+
+
